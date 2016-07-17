b866ca89b1385bfb8002e45ccc37c34a5cccae8d
bhupendra11 <bhupendrashekhawat1108@gmail.com>
2016-07-17 15:59:36 +0530

    Add java library to tell jokes

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,11 +1,13 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
+import com.example.Joker;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -40,7 +42,11 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+
+        Joker joker = new Joker();
+        String joke =joker.getJoke();
+
+        Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ joketeller/src/main/java/com/example/Joker.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Joker{
+
+    public String getJoke(){
+        return "Now thats a good joke !";
+    }
+}
